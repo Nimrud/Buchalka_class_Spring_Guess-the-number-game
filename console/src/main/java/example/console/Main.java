e25480833a342c0e851b034c23af6c4313f622e0
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-08-04 19:23:15 +0200

    14: constructor injection + code cleanup

--- console/src/main/java/example/console/Main.java
+++ console/src/main/java/example/console/Main.java
@@ -1,9 +1,6 @@
 package example.console;
 
 import example.config.AppConfig;
-import example.Game;
-import example.MessageGenerator;
-import example.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,34 +13,9 @@ public class Main {
     public static void main(String[] args) {
         log.info("Guess the Number Game");
 
-        // tworzymy kontekst (Container)
         ConfigurableApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // wyciągamy z tego kontekstu beana (szukamy odpowiedniego po ID)
-        // kontener da nam nową instancję klasy NumberGeneratorImpl (info z beana),
-        // a sporo NumberGeneratorImpl implementuje interfejs NumberGenerator, to
-        // kodujemy to do interface'u (dobra praktyka)
-        NumberGenerator numberGenerator =
-                context.getBean(NumberGenerator.class);
-
-        // wywołujemy metodę:
-        int number = numberGenerator.next();
-
-        // log:
-        log.info("number = {}", number);
-            // slf4j automatycznie zamieni {} na parametr podany po przecinku
-
-        // pobieramy beana gry z kontekstu (kontenera)
-        Game game = context.getBean(Game.class);
-
-        // CHALLENGE 1:
-        MessageGenerator messageGenerator =
-                context.getBean(MessageGenerator.class);
-        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
-        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
-
-        // zamykamy kontener:
         context.close();
     }
 }
