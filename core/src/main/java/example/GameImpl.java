cbec7f1dc7a73794cdf9276783f5d9d4dec76b6a
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-08-05 19:46:27 +0200

    16: Lombok (2) + mini-challenge

--- core/src/main/java/example/GameImpl.java
+++ core/src/main/java/example/GameImpl.java
@@ -1,28 +1,33 @@
 package example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game{
 
-    // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
-
     // == fields ==
+    @Getter(AccessLevel.NONE)     // dla tego pola nie zostanie utworzony getter
     private final NumberGenerator numberGenerator;
+
     private final int guessCount;       // ile razy można zgadywać
     private int number;           // wartość losowo wygenerowanej przez apkę liczby
-    private int guess;            // "strzał" gracza
     private int smallest;         // smallest, biggest - zakres liczb, pomiędzy którymi jest wygenerowana liczba
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter
+    private int guess;            // "strzał" gracza
 
     // == constructors ==
     @Autowired
@@ -51,41 +56,6 @@ public class GameImpl implements Game{
     }
 
     // == public methods ==
-    @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
-
     @Override
     public void check() {
         checkValidNumberRange();
